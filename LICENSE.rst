8388d388a4748796698860089e64add78738aeb2
Krzysztof Kaszubski <krzysztof.kaszubski@diehl.com>
2018-10-31 13:42:24 +0100

    Theme rst added.

--- LICENSE.rst
+++ LICENSE.rst
@@ -1,3 +1,7 @@
+*******
+License
+*******
+
 GNU General Public License
 ==========================
 
--- docs/conf.py
+++ docs/conf.py
@@ -75,13 +75,27 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
